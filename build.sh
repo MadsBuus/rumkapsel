382f2a295cd39aa51b19a38879a4d6f39cfb9eba
Mads Buus <mads.buus@tattoodo.com>
2026-09-09 22:09:52 +0200

    Release 0.15

--- build.sh
+++ build.sh
@@ -21,8 +21,8 @@ cat > "$APP/Contents/Info.plist" <<PLIST
   <key>CFBundleExecutable</key><string>rumkapsel</string>
   <key>CFBundleIconFile</key><string>AppIcon</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>0.14</string>
-  <key>CFBundleVersion</key><string>202609092129</string>
+  <key>CFBundleShortVersionString</key><string>0.15</string>
+  <key>CFBundleVersion</key><string>202609092208</string>
   <key>LSMinimumSystemVersion</key><string>14.0</string>
   <key>NSHighResolutionCapable</key><true/>
   <key>SUFeedURL</key><string>https://raw.githubusercontent.com/MadsBuus/rumkapsel-releases/main/appcast.xml</string>
