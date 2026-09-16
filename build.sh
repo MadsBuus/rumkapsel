0a48c8bb2d328f5d0d8c898d438a9e38ee5e2a8c
Mads Buus <mads.buus@tattoodo.com>
2026-09-17 00:27:47 +0200

    Release 0.40

--- build.sh
+++ build.sh
@@ -41,8 +41,8 @@ cat > "$APP/Contents/Info.plist" <<PLIST
   <key>CFBundleExecutable</key><string>rumkapsel</string>
   <key>CFBundleIconFile</key><string>AppIcon</string>
   <key>CFBundlePackageType</key><string>APPL</string>
-  <key>CFBundleShortVersionString</key><string>0.37</string>
-  <key>CFBundleVersion</key><string>202609161425</string>
+  <key>CFBundleShortVersionString</key><string>0.40</string>
+  <key>CFBundleVersion</key><string>202609170024</string>
   <key>RKBuild</key><string>$SHA · $SUBJ</string>
   <key>LSMinimumSystemVersion</key><string>14.0</string>
   <key>NSHighResolutionCapable</key><true/>
